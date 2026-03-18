7d0b52931a17c4e2812900095ba8b17f96b3dd1a
Yukari1907 <raissa.yukari15@gmail.com>
2026-03-17 22:01:02 -0300

    Feat: correcao de erro controller

--- backend_consultas/src/main/java/com/fiap/ec/backend_consultas/service/EspecialidadeService.java
+++ backend_consultas/src/main/java/com/fiap/ec/backend_consultas/service/EspecialidadeService.java
@@ -6,13 +6,21 @@ import java.util.List;
 @Service
 public class EspecialidadeService {
     private final EspecialidadeRepository repository;
+
     public EspecialidadeService(EspecialidadeRepository repository) {
         this.repository = repository;
     }
+
     public Especialidade salvar(Especialidade especialidade) {
         return repository.save(especialidade);
     }
+
     public List<Especialidade> listar() {
         return repository.findAll();
+    }
+
+    public Especialidade buscarPorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Especialidade não encontrada"));
     }
 }
